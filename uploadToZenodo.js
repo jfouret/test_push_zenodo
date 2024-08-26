38f1b6b5e8d550b4cfa1c09bf807568e23418b1e
jfouret <julien@fouret.me>
2024-08-26 15:53:18 +0200

    test with retry

--- uploadToZenodo.js
+++ uploadToZenodo.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const argv = require('yargs').argv;
 const path = require('path');
 const mime = require('mime-types');
+const axiosRetry = require('axios-retry').default;
+axiosRetry(axios, { retries: 3 });
 
 // Configuration
 const zenodo_url = 'https://sandbox.zenodo.org';
